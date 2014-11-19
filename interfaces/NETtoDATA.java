379546677ed45878e824d2a01e0aeb359ce418ac
Yoann <yoann.chaumin@gmail.com>
2014-11-19 18:04:55 +0100

    Mise à jour de l'interface NETtoDATA après certation avec NET

--- interfaces/NETtoDATA.java
+++ interfaces/NETtoDATA.java
@@ -8,24 +8,73 @@ import DATA.model.Picture;
 import DATA.model.User;
 
 public interface NETtoDATA {
+	
+	/**
+	 * Send a comment to a remote picture. 
+	 * @param comment
+	 */
 	public void addComment(Comment comment) ;
 	
-
+	/**
+	 * Return the list of saved IPs.
+	 * @return List<Inet4Address>
+	 */
 	public List<Inet4Address> getConnectedIps() ;
 	
+	/**
+	 * Return current local user. 
+	 * @return User
+	 */
 	public User getProfil() ;
 	
+	/**
+	 * Send information about remote User.
+	 * @param user
+	 * @param idRequest
+	 */
 	public void infoUser(User user, int idRequest) ;
 	
-
-	public void receiveFriendRequest(long idSender) ;
+	/**
+	 * Inform that the current User has a friend request.
+	 * @param idSender
+	 */
+	public void receiveFriendRequest(User user) ;
 	
+	/**
+	 * Inform that the current User of friend response.
+	 * @param idSender
+	 */
+	public void receiveFriendResponse(User user) ;
 	
+	/**
+	 * Send list pictures asked by the User. 
+	 * @param pictures
+	 * @param idRequest
+	 */
 	public void resultPictures(List<Picture> pictures, int idRequest) ;
 
+	/**
+	 * Send picture acked by the User.
+	 * @param picture
+	 * @param pageId
+	 */
+	public void sendPicture(Picture picture, int pageId) ;
 
-	public void sendPictureToPage(Picture picture, int pageId) ;
-
-
-	public void setConnectedUser(User user) ;
+	/**
+	 * Send connected user.
+	 * @param user
+	 */
+	public void HelloUser(User user) ;
+	
+	/**
+	 * Inform that user is unconnected.
+	 * @param user
+	 */
+	public void GoodByeUser(User user) ;
+	
+	/**
+	 * Inform that your friend remove you to his friend.
+	 * @param user
+	 */
+	public void notFriendAnymore(User user) ;
 }
