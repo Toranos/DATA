30f562ddb44416098acd2379b392d4f25239769e
Bruno Da Silva <dasilvab42@gmail.com>
2014-11-25 16:56:31 +0100

    Connection et getAllUsers

--- services/UserService.java
+++ services/UserService.java
@@ -26,35 +26,48 @@ public class UserService {
 	 * @throws BadInformationException	When incorrect information found
 	 */
 	public User createUser(User u) throws BadInformationException {
-		if(u == null)
+		if (u == null) {
 			throw new BadInformationException("User is null");
-		if(!checkCredentialNotEmpty(u.getLogin(), u.getPassword()))
+		}
+		if (!checkCredentialNotEmpty(u.getLogin(), u.getPassword())) {
 			throw new BadInformationException("Login/password empty");
-		if(u.getListIP() == null || u.getListIP().isEmpty())
+		}
+		if (u.getListIP() == null || u.getListIP().isEmpty()) {
 			throw new BadInformationException("Password is empty");
+		}
 		return u;
 	}
 	
 	/**
 	 * Check if the user information is correct for sign in
-	 * @param u	The user to check
+	 * @param username Username for the user
+	 * @param password Password for the user
 	 * @return	The user with correct information
 	 * @throws BadInformationException	When incorrect information found
 	 */
-	public User checkProfile(User u) throws BadInformationException {
-		User currentUser = createUser(u);
-		
-		if (currentUser.getLogin().equals(data.getUser().getLogin())
-		&& currentUser.getPassword().equals(data.getUser().getPassword())) {
+	public User checkProfile(String username, String password) throws BadInformationException {
+		if (!checkCredentialNotEmpty(username, password)) {
+			throw new BadInformationException("Login/password empty");
+		}
+		if (username.equals(data.getUser().getLogin())
+		&& username.equals(data.getUser().getPassword())) {
 			return data.getUser();
 		}
-		return currentUser;
-
-	public boolean checkCredentialNotEmpty(String login, String password) {
-		if(login == null || login.equals("")){
+		return null;
+	}
+		
+	/**
+	 * Check if the credentials are not empty
+	 * @param username Username for the user
+	 * @param password Password for the user
+	 * @return boolean to check not null credentials
+	 * @throws BadInformationException	When incorrect information found
+	 */
+	public boolean checkCredentialNotEmpty(String username, String password) {
+		if (username == null || username.equals("")) {
 			return false;
 		}
-		if(password == null || password.equals("")){
+		if (password == null || password.equals("")) {
 			return false;
 		}
 		return true;
