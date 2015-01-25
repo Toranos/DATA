01985fb47cf95945a99b4b1e6efb714c781c41e5
Bruno Da Silva <dasilvab42@gmail.com>
2015-01-25 18:04:49 +0100

    uid on picture

--- model/Picture.java
+++ model/Picture.java
@@ -104,6 +104,7 @@ public class Picture implements Serializable {
     	for(Rule rule : p.getListRules()){
     		this.listRules.add(rule);
     	}
+    	this.uid = uid;
     	this.user = p.user;
     }
     
